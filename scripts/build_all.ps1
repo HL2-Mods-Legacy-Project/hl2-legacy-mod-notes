f159934b811afe455b396e16246be49745d80783
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-08-03 12:20:25 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,9 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
-  -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -Version "1.0.0" `
+  -OutputFilePrefix "Notes" `
+  -ModName "Notes: Surrender Cut" `
+  -ModFolder "notes" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/notes/Notes-Original.zip" `
+  -ModBaseFilesUrlHash "ff6795e5572d61397233d9c050c28ce42e97a7a8b5fd38457f4f9e7b0cf0d955" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
